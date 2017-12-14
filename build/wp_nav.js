859ed02f43b8bf034b4c79c18596457aefd0f501
lemehovskiy <lemehovskiy@gmail.com>
2017-12-14 21:25:52 +0200

    build

---
+++ build/wp_nav.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+;(function (factory) {
+    'use strict';
+
+    if (typeof define === 'function' && define.amd) {
+        define(['jquery'], factory);
+    } else if (typeof exports !== 'undefined') {
+        module.exports = factory(require('jquery'));
+    } else {
+        factory(jQuery);
+    }
+})(function ($) {
+    'use strict';
+
+    var WpNav = function WpNav(element, options) {
+        _classCallCheck(this, WpNav);
+
+        var self = this;
+
+        self.settings = $.extend({}, options);
+
+        var $element = $(element);
+    };
+
+    $.fn.wpNav = function () {
+        var $this = this,
+            opt = arguments[0],
+            args = Array.prototype.slice.call(arguments, 1),
+            length = $this.length,
+            i = void 0,
+            ret = void 0;
+        for (i = 0; i < length; i++) {
+            if ((typeof opt === 'undefined' ? 'undefined' : _typeof(opt)) == 'object' || typeof opt == 'undefined') $this[i].wp_nav = new WpNav($this[i], opt);else ret = $this[i].wp_nav[opt].apply($this[i].wp_nav, args);
+            if (typeof ret != 'undefined') return ret;
+        }
+        return $this;
+    };
+});
