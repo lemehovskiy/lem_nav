5c754238294aa2a6e6b36b1cb0e5e585d905c6f6
lemehovskiy <lemehovskiy@gmail.com>
2017-12-15 19:46:36 +0200

    demo nav

--- build/wp_nav.js
+++ build/wp_nav.js
@@ -2,6 +2,8 @@
 
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 ;(function (factory) {
@@ -17,15 +19,204 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 })(function ($) {
     'use strict';
 
-    var WpNav = function WpNav(element, options) {
-        _classCallCheck(this, WpNav);
+    var WpNav = function () {
+        function WpNav(element, options) {
+            _classCallCheck(this, WpNav);
 
-        var self = this;
+            var self = this;
 
-        self.settings = $.extend({}, options);
+            self.nav = {};
 
-        var $element = $(element);
-    };
+            self.nav.dropdowns = [];
+
+            self.nav.navbar_open = false;
+
+            self.$navbar = $(element);
+
+            self.settings = $.extend({
+                navbar_toggle: '.wp-navbar-toggle',
+                collapse_duration: 0.2,
+                trigger: 'click',
+                trigger_linked: false,
+                extra_trigger_button: "<button class='extra-trigger'>+</button>",
+                navbar_collapse_duration: 0.5,
+                on_dropdown_shown: function on_dropdown_shown() {},
+                on_dropdown_hide: function on_dropdown_hide() {}
+
+            }, options);
+
+            self.init();
+        }
+
+        _createClass(WpNav, [{
+            key: 'init',
+            value: function init() {
+                var self = this;
+
+                self.$navbar.find('.menu-item-has-children').each(function () {
+                    var $this = $(this);
+
+                    self.nav.dropdowns.push({
+                        trigger: $this,
+                        menu: $this.find('>.sub-menu'),
+                        menu_items: $this.find('>.sub-menu >li'),
+                        open: false
+                    });
+                });
+
+                self.nav.dropdowns.forEach(function (dropdown) {
+
+                    dropdown.menu_items_tl = new TimelineMax({
+                        paused: true
+                    });
+
+                    dropdown.menu_items_tl.staggerFrom(dropdown.menu_items, self.settings.collapse_duration, {
+                        autoAlpha: 0
+                    }, self.settings.collapse_duration / dropdown.menu_items.length);
+                });
+
+                if (self.settings.trigger_linked) {
+                    self.extra_trigger();
+                }
+
+                if (self.settings.trigger == 'click') {
+                    $(window).click(function () {
+                        self.close_all();
+                    });
+                    self.nav.dropdowns.forEach(function (dropdown) {
+                        dropdown.trigger.on('click', function (event) {
+                            event.stopPropagation();
+                            if (dropdown.open) {
+                                self.close({
+                                    dropdown: dropdown
+                                });
+                            } else {
+                                self.close_all();
+                                self.open({
+                                    dropdown: dropdown
+                                });
+                            }
+                        });
+                    });
+                } else if (self.settings.trigger == 'hover') {
+                    self.nav.dropdowns.forEach(function (dropdown) {
+                        dropdown.trigger.hover(function () {
+                            self.open({
+                                dropdown: dropdown
+                            });
+                        }, function () {
+                            self.close({
+                                dropdown: dropdown
+                            });
+                        });
+                    });
+                }
+
+                self.navbar_collapse();
+            }
+        }, {
+            key: 'close_all',
+            value: function close_all() {
+
+                var self = this;
+
+                self.nav.dropdowns.forEach(function (dropdown) {
+                    if (dropdown.open) {
+                        self.close({
+                            dropdown: dropdown
+                        });
+                    }
+                });
+            }
+        }, {
+            key: 'navbar_collapse',
+            value: function navbar_collapse() {
+                var self = this;
+
+                self.nav.navbar_trigger = $(self.settings.navbar_toggle);
+
+                self.nav.navbar_trigger.on('click', function () {
+                    if (self.nav.navbar_open) {
+                        TweenLite.to(self.$navbar, self.settings.navbar_collapse_duration, { height: 0 });
+
+                        self.nav.navbar_open = false;
+                        self.nav.navbar_trigger.removeClass('open');
+                    } else {
+                        TweenLite.set(self.$navbar, { height: "auto" });
+                        TweenLite.from(self.$navbar, self.settings.navbar_collapse_duration, { height: 0 });
+
+                        self.nav.navbar_open = true;
+                        self.nav.navbar_trigger.addClass('open');
+                    }
+                });
+            }
+        }, {
+            key: 'extra_trigger',
+            value: function extra_trigger() {
+                var self = this;
+
+                self.nav.dropdowns.forEach(function (dropdown) {
+
+                    var $button = $(self.settings.extra_trigger_button);
+
+                    dropdown.trigger.append($button);
+
+                    $button.on('click', function () {
+                        if (dropdown.open) {
+                            self.close({
+                                dropdown: dropdown
+                            });
+                        } else {
+                            self.open({
+                                dropdown: dropdown
+                            });
+                        }
+                    });
+                });
+            }
+        }, {
+            key: 'open',
+            value: function open(options) {
+                var self = this;
+                var dropdown = options.dropdown;
+                var current_menu_height = dropdown.menu.outerHeight();
+
+                TweenLite.set(dropdown.menu, { height: "auto" });
+                TweenLite.from(dropdown.menu, self.settings.collapse_duration, {
+                    height: current_menu_height,
+                    onComplete: dropdown_shown
+                });
+
+                dropdown.menu_items_tl.timeScale(1.2);
+                dropdown.menu_items_tl.play();
+
+                dropdown.open = true;
+                dropdown.trigger.addClass('open');
+
+                function dropdown_shown() {
+                    self.settings.on_dropdown_shown();
+                }
+            }
+        }, {
+            key: 'close',
+            value: function close(options) {
+                var self = this;
+                var dropdown = options.dropdown;
+
+                self.settings.on_dropdown_hide();
+
+                dropdown.menu_items_tl.timeScale(8);
+                dropdown.menu_items_tl.reverse();
+
+                TweenLite.to(dropdown.menu, self.settings.collapse_duration, { height: 0 });
+
+                dropdown.open = false;
+                dropdown.trigger.removeClass('open');
+            }
+        }]);
+
+        return WpNav;
+    }();
 
     $.fn.wpNav = function () {
         var $this = this,
--- src/wp_nav.es6
+++ src/wp_nav.es6
@@ -18,13 +18,207 @@
         constructor(element, options) {
 
             let self = this;
-            
+
+            self.nav = {};
+
+            self.nav.dropdowns = [];
+
+            self.nav.navbar_open = false;
+
+            self.$navbar = $(element);
+
             self.settings = $.extend({
-               
+                navbar_toggle: '.wp-navbar-toggle',
+                collapse_duration: 0.2,
+                trigger: 'click',
+                trigger_linked: false,
+                extra_trigger_button: "<button class='extra-trigger'>+</button>",
+                navbar_collapse_duration: 0.5,
+                on_dropdown_shown: function () {
+                },
+                on_dropdown_hide: function () {
+                }
+
             }, options);
 
-            let $element = $(element);
-            
+            self.init();
+        }
+
+        init() {
+            let self = this;
+
+            self.$navbar.find('.menu-item-has-children').each(function () {
+                let $this = $(this);
+
+                self.nav.dropdowns.push({
+                    trigger: $this,
+                    menu: $this.find('>.sub-menu'),
+                    menu_items: $this.find('>.sub-menu >li'),
+                    open: false
+                });
+            })
+
+
+            self.nav.dropdowns.forEach(function (dropdown) {
+
+                dropdown.menu_items_tl = new TimelineMax({
+                    paused: true
+                });
+
+
+                dropdown.menu_items_tl.staggerFrom(
+                    dropdown.menu_items,
+                    self.settings.collapse_duration,
+                    {
+                        autoAlpha: 0
+                    }, self.settings.collapse_duration / dropdown.menu_items.length)
+
+            })
+
+
+            if (self.settings.trigger_linked) {
+                self.extra_trigger();
+            }
+
+            if (self.settings.trigger == 'click') {
+                $(window).click(function () {
+                    self.close_all();
+                });
+                self.nav.dropdowns.forEach(function (dropdown) {
+                    dropdown.trigger.on('click', function (event) {
+                        event.stopPropagation();
+                        if (dropdown.open) {
+                            self.close({
+                                dropdown: dropdown
+                            })
+                        }
+                        else {
+                            self.close_all();
+                            self.open({
+                                dropdown: dropdown
+                            })
+                        }
+                    })
+                })
+            }
+
+            else if (self.settings.trigger == 'hover') {
+                self.nav.dropdowns.forEach(function (dropdown) {
+                    dropdown.trigger.hover(
+                        function () {
+                            self.open({
+                                dropdown: dropdown
+                            })
+                        },
+                        function () {
+                            self.close({
+                                dropdown: dropdown
+                            })
+                        }
+                    )
+                })
+            }
+
+
+            self.navbar_collapse();
+        }
+
+        close_all() {
+
+            let self = this;
+
+            self.nav.dropdowns.forEach(function (dropdown) {
+                if (dropdown.open) {
+                    self.close({
+                        dropdown: dropdown
+                    })
+                }
+            })
+        }
+
+        navbar_collapse() {
+            let self = this;
+
+            self.nav.navbar_trigger = $(self.settings.navbar_toggle);
+
+
+            self.nav.navbar_trigger.on('click', function () {
+                if (self.nav.navbar_open) {
+                    TweenLite.to(self.$navbar, self.settings.navbar_collapse_duration, {height: 0})
+
+                    self.nav.navbar_open = false;
+                    self.nav.navbar_trigger.removeClass('open');
+                }
+
+                else {
+                    TweenLite.set(self.$navbar, {height: "auto"})
+                    TweenLite.from(self.$navbar, self.settings.navbar_collapse_duration, {height: 0})
+
+                    self.nav.navbar_open = true;
+                    self.nav.navbar_trigger.addClass('open');
+                }
+            })
+        }
+
+        extra_trigger() {
+            let self = this;
+
+            self.nav.dropdowns.forEach(function (dropdown) {
+
+                let $button = $(self.settings.extra_trigger_button);
+
+                dropdown.trigger.append($button);
+
+                $button.on('click', function () {
+                    if (dropdown.open) {
+                        self.close({
+                            dropdown: dropdown
+                        })
+                    }
+                    else {
+                        self.open({
+                            dropdown: dropdown
+                        })
+                    }
+                })
+            })
+        }
+
+        open(options) {
+            let self = this;
+            let dropdown = options.dropdown;
+            let current_menu_height = dropdown.menu.outerHeight();
+
+            TweenLite.set(dropdown.menu, {height: "auto"})
+            TweenLite.from(dropdown.menu, self.settings.collapse_duration, {
+                height: current_menu_height,
+                onComplete: dropdown_shown
+            })
+
+            dropdown.menu_items_tl.timeScale(1.2);
+            dropdown.menu_items_tl.play();
+
+            dropdown.open = true;
+            dropdown.trigger.addClass('open');
+
+            function dropdown_shown() {
+                self.settings.on_dropdown_shown()
+            }
+        }
+
+        close(options) {
+            let self = this;
+            let dropdown = options.dropdown;
+
+            self.settings.on_dropdown_hide()
+
+            dropdown.menu_items_tl.timeScale(8);
+            dropdown.menu_items_tl.reverse();
+
+            TweenLite.to(dropdown.menu, self.settings.collapse_duration, {height: 0})
+
+            dropdown.open = false;
+            dropdown.trigger.removeClass('open');
         }
     }
 
